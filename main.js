cd86c582097b1c95ffb7eca7145710b68608c4d1
Daniel Aguilar <dani@protozoo.com>
2023-01-04 20:27:34 +0100

    tweaking

--- main.js
+++ main.js
@@ -70,7 +70,7 @@ function setup(){
     // seed
     fxrand = sfc32(...hashes)
 
-    margin = (W+H)/6;
+    margin = (W+H)/2;
     colors = [];
     colors.push(["4d0838","f3722c","f8961e","f9844a","f9c74f","90be6d","43aa8b","4d908e","577590","277da1"])
     colors.push(["001219","005f73","0a9396","94d2bd","e9d8a6","ee9b00","ca6702","bb3e03","ae2012","9b2226"])
